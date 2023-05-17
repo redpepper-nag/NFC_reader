1506e2e9937cf512ec32cc71509e4051347d9350
Louise Naginski <louisindiana@gmail.com>
2023-05-17 11:20:25 +0200

    Made NFC read/write buttons

--- App.js
+++ App.js
@@ -1,46 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Switch } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  
-  const [isNight, setIsNight] = useState(false);
-  const toggleSwitch = () => setIsNight(prev => !prev)
 
   return (
-    <View style={isNight ? styles.containerNight: styles.containerDay}>
-      <Text style={isNight ? styles.textNight : styles.textDay}>{isNight ? 'Ciao' : 'Yo mundo'}</Text>
-      <Switch 
-      trackColor={{false: "#767577", true: '#81b0ff'}}
-      thumbColor={isNight ? '#f5dd4b' : '#f4f3f4'}
-      ios_backgroundColor="#3e3e3e"
-      onValueChange={toggleSwitch}
-      value="isNight"
-      />
+    <View style={styles.container}>
+      <Text style={styles.title}>NFC reader</Text>
+      <Button style={styles.button} title="Read" />
+      <Button style={styles.button} title="Write" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  containerDay: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#e3ce9a'
   },
-  containerNight: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#1d3e63'
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
-  textDay: {
-    color: 'black',
-    fontSize: 32,
-  },
-  textNight: {
-    color: 'white',
-    fontSize: 32,
-  },
+  button: {
+  }
 });
